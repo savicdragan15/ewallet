ec18d035327723838fd209fe5121d7ac86d2e34b
Dragan Savic <savicdragan2707@gmail.com>
2018-11-18 16:47:33 +0100

    test commit

--- app/Http/Controllers/Api/V1/DashboardController.php
+++ app/Http/Controllers/Api/V1/DashboardController.php
@@ -12,9 +12,7 @@ use App\Http\Controllers\Controller;
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * 
      */
     public function index()
     {
