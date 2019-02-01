ecc15d4afed3304e28bcf5ce6012d5ab1b589521
Dragan <savicdragan2707@gmail.com>
2019-02-01 14:54:01 +0100

    - fix

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -246,11 +246,13 @@ class Order extends BaseModel
     public function getSumOrdersByMonth(User $user, $limit = 12)
     {
         return \DB::table('orders')
-            ->select(\DB::raw('SUM(amount) as sum, DATE_FORMAT(created_at, "%b %Y") as month'))
+            ->select(\DB::raw('SUM(amount) as sum, DATE_FORMAT(created_at, "%b %Y") as month, DATE_FORMAT(created_at, "%m") as number_month, DATE_FORMAT(created_at, "%Y") as year'))
             ->where('user_id', $user->id)
             ->whereRaw('created_at >= LAST_DAY(NOW()) + INTERVAL 1 DAY - INTERVAL '.$limit.' MONTH')
             ->whereNull($this->getDeletedAtColumn())
-            ->groupBy('month')
+            ->groupBy('month', 'year', 'number_month')
+            ->orderBy('year', 'ASC')
+            ->orderBy('number_month', 'ASC')
             ->get();
     }
 }
